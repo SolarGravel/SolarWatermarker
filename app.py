bc4e5b72204cdb591963ce198ede9f12f791c696
SolarGravel <astridneb@gmail.com>
2025-11-02 20:16:07 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,43 @@
+from tkinter import Tk, ttk, filedialog, StringVar
+
+root = Tk()
+root.title("Add Watermark")
+
+img_path: StringVar = StringVar()
+
+
+def get_image_file(event=None):
+    img_path.set(filedialog.askopenfilename(parent=root,filetypes=[("Image Files", ["*.png", "*.jpg", "*.jpeg", "*.webp"])]))
+
+
+def get_watermark_folder(event=None):
+    return filedialog.asksaveasfilename(parent=root, defaultextension="*.png")
+
+
+mainframe = ttk.Frame(root, padding=(3, 3, 12, 12))
+mainframe.grid(column=0, row=0, sticky="NWES")
+
+img_label = ttk.Label(mainframe, text="Upload an image to get watermarked.").grid(
+    column=0, row=0, sticky="we", columnspan=2
+)
+img_button = ttk.Button(mainframe, text="Upload Image", command=get_image_file).grid(
+    column=0, row=1, sticky="we"
+)
+watermark_button = ttk.Button(
+    mainframe, text="Watermark", command=get_watermark_folder
+).grid(column=1, row=1, sticky="we")
+file_label = ttk.Label(mainframe, text="Selected Image: ").grid(
+    column=0, row=2, sticky="w"
+)
+path_label = ttk.Label(mainframe, textvariable=img_path).grid(
+    column=1, row=2, sticky="w"
+)
+
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
+mainframe.columnconfigure(2, weight=1)
+
+for child in mainframe.winfo_children():
+    child.grid_configure(padx=5, pady=5)
+
+root.mainloop()
